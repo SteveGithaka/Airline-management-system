7082ee4aae28f78a8c8ed723d1714b77a8d93888
SteveGithaka <githakasteve26@gmail.com>
2025-04-19 20:38:01 +0300

    Third commit

--- airline-management-system.c
+++ airline-management-system.c
@@ -91,7 +91,96 @@ void viewdetails(void)
 {
 	
 	system("cls");
-	
-	
+printf("--------------------------------------------------------------------------------------------------------------------------------------------------------");
+	printf("\nFlight.No\t Name\t\t\t\t Destinations          |\t\t\t Charges \t\t Date     \t\t  Duration\n");
+	printf("--------------------------------------------------------------------------------------------------------------------------------------------------------");
+	printf("\n1001\t\t Flight 01\t\t\t Kenya To Tanzania     |\t\t\tKsh 30000  \t\t 01 Apr 2025\t\t 2hrs ");
+	printf("\n1002\t\t Flight 02\t\t\t Kenya To Uganda       |\t\t\tKsh 40000  \t\t 02 Apr 2025\t\t 2hrs ");
+	printf("\n1003\t\t Flight 03\t\t\t Kenya To Botswana     |\t\t\tKsh 50000  \t\t 03 Apr 2025\t\t 4hrs ");
+	  printf("\n1004\t\t Flight 04\t\t\t Kenya To Algeria      |\t\t\tKsh 60000  \t\t 04 Apr 2025\t\t 5hrs ");
+	printf("\n1005\t\t Flight 05\t\t\t Kenya To Angola       |\t\t\tKsh 70000  \t\t 05 Apr 2025\t\t 6hrs ");
+	printf("\n1006\t\t Flight 06\t\t\t Kenya To Egypt        |\t\t\tKsh 80000  \t\t 06 Apr 2025\t\t 6hrs ");
+	   printf("\n1007\t\t Flight 07\t\t\t Kenya To Malawi       |\t\t\tKsh 90000  \t\t 04 Apr 2025\t\t 7hrs ");
+	  printf("\n1008\t\t Flight 08\t\t\t Kenya To South Sudan  |\t\t\tKsh 100000 \t\t 08 Apr 2025\t\t 8hrs "); 
+	  printf("\n1009\t\t Flight 09\t\t\t Kenya To Nigeria      |\t\t\tKsh 110000 \t\t 04 Apr 2025\t\t 9hrs");
+	 printf("\n1010\t\t Flight 10\t\t\t Kenya To South Africa |\t\t\tKsh 120000 \t\t 02 Apr 2025\t\t 10hrs");
+
+
+}
+
+
+
+void reservationA(void)
+{
+
+char confirm;
+int i=0;
+float charges;
+
+pd passdetails;
+FILE *fp1;
+fp1=fopen("Kenya.txt","a");
+system("cls");
+
+printf("\nEnter Your Name :> ");
+fflush(stdin);
+gets(passdetails.name);
+
+printf("\n\n>>Press Enter To View Available flights >> ");
+getch();
+system("cls");
+viewdetails();
+
+printf("\n\nEnter flight number :> ");
+start1:
+scanf("%d",&passdetails.flight_num);
+
+if(passdetails.flight_num>=1001 && passdetails.flight_num<=1010)
+{
+charges=charge(passdetails.flight_num);
+printticket(passdetails.name,passdetails.flight_num,charges);
+}
+
+else
+{
+
+printf("\nInvalid Flight Number!");
+printf("\n Enter again --> ");
+goto start1;
+
+}
+
+printf("\n\nConfirm Ticket ('y' for yes/'n' for no) :>");
+start:
+scanf(" %c",&confirm);
+
+if(confirm == 'y')
+{
+fprintf(fp1,"%s\t\t%d\t\t%.2f\n",&passdetails.name,passdetails.flight_num,charges);
+printf("\n****************************************");
+printf("\n RESERVATION DONE.......HAPPY JOURNEY :)");
+printf("\n****************************************\n");
+printf("\nPress any key to go back to Main menu");
+}
+
+else
+{
+if(confirm=='n')
+{
+	printf("\nSorry to see you go :( ");
+	printf("\nReservation cancelled...");
+	printf("\nPress any key to go back to  Main menu!");
+}
+
+else
+{
+	printf("\nInvalid choice entered! Enter again -----> ");
+	goto start;
+}
+
+}
+fclose(fp1);
+getch();
+
 
 }
